de08d8bc7944e546efce331bf88d300a11f6888d
mekymeky <mekymeky@users.noreply.github.com>
2020-07-25 22:47:54 +0200

    Added startup retry interval

--- runbot.py
+++ runbot.py
@@ -1,4 +1,13 @@
+import time
 import seedsbot.main as seedsbot
 
+RETRY_INTERVAL = 5
+
 if __name__ == "__main__":
-    seedsbot.run()
+    while True:
+        try:
+            seedsbot.run()
+        except Exception as err:
+            print(err)
+            print("Retrying in", RETRY_INTERVAL)
+            time.sleep(RETRY_INTERVAL)
